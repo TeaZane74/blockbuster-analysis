b58619431cd89ea021142ac802f1c800ba0684fb
Nicolas Moine <62545026+TeaZane74@users.noreply.github.com>
2023-08-03 15:25:02 +0200

    update

--- src/data.py
+++ src/data.py
@@ -6,6 +6,9 @@ p = pathlib.Path('.')
 path_name = {path:path.name.replace('tbl','').replace('.csv', '') for path in list(p.glob('**/*.csv'))}
 tables = {path.name.replace('tbl','').replace('.csv', ''):pd.read_csv(path, index_col=f'{path_name[path]}ID') for path in path_name}
 
+
+tables['Film'] = tables[tables['Film'][[['FilmBoxOfficeDollars']]].isna() == False]
+
 tables['Film']['FilmReleaseDate'] = pd.to_datetime(tables['Film']['FilmReleaseDate'])
 tables['Film']['FilmBoxOfficeDollars'] = tables['Film']['FilmBoxOfficeDollars'] /10**6
 tables['Film']['FilmBudgetDollars'] = tables['Film']['FilmBudgetDollars'] /10**6
